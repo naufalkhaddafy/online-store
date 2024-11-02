4caeeb4f8a9aead14d246dbc3bd39c5b06fff04e
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2024-11-02 19:43:38 +0800

    setup: interface raja ongkir as provider

---
+++ app/Contracts/RajaOngkir/LocationInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Contracts\RajaOngkir;
+
+use App\Models\City;
+use App\Models\Province;
+
+interface LocationInterface
+{
+    public function getProvinces();
+    public function getCities(Province $province);
+    public function getSubdistrict(City $city);
+}
